02f1c0549ea1766ae535ad81ccdb5e0aa6437698
Qianqian Fang <fangq@nmr.mgh.harvard.edu>
2020-01-28 01:20:46 -0500

    initial version of the JSON formatted genomic data

--- src/gbff2json.pl
+++ src/gbff2json.pl
@@ -1,4 +1,15 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl 
+###############################################################################
+#
+# A GeneBank GBFF file to JSON file converter
+#
+# Author:  Qianqian Fang <q.fang at neu.edu>
+# License: BSD 3-clause
+# Version: 0.4
+# URL:     http://openjdata.org
+# Github:  https://github.com/fangq/gene2019ncov/
+#
+###############################################################################
 
 use strict;
 use warnings;
@@ -11,53 +22,64 @@ if($#ARGV<0){
 	exit 0;
 }
 
-my (%obj1, %obj2, %obj3);
 my ($key1, $key2, $key3)=("","","");
-my ($lastobj,$name,$value);
+my ($lastobj,$value);
 
-tie %obj1, "Tie::IxHash";
-tie %obj2, "Tie::IxHash";
-tie %obj3, "Tie::IxHash";
+tie my %obj1, "Tie::IxHash";
+tie my %obj2, "Tie::IxHash";
+tie my %obj3, "Tie::IxHash";
 
 while(<>){
 	next if(/^\s*$/);
 	last if(/^\/\/$/);
-	if(/^((\s*)(\S+)\s+)(.*)$/){
-		$name=$3;
-		$value=$4;
+	if(/^((\s*)(\S+)(\s+))(.*)/){
+		my $ln=$_;
+		$value=$4.$5;
 		if(length($2)==0){
-			push(@{$obj1{$3}}, $4);
-			if($key2 ne ""){
-				push(@{$obj1{$key1}},rmap(%obj2));
-				%obj2=();
-				$key2="";
-			}
-			$key1=$3;
-			$lastobj=$obj1{$3};
-		}elsif(length($2)<12){
-			push(@{$obj2{$3}}, $4);
-			if($key3 ne ""){
-				push(@{$obj1{$key2}},rmap(%obj3));
+			if((keys %obj3)>0){
+				push(@{$obj2{$key2}},rmap(\%obj3));
 				%obj3=();
 				$key3="";
 			}
+			if((keys %obj2)>0){
+				push(@{$obj1{$key1}},rmap(\%obj2));
+				%obj2=();
+				$key2="";
+			}
+			push(@{$obj1{$3}}, $5);
+			$key1=$3;
+			$lastobj=$obj1{$3};
+		}elsif(length($2)<12){
+			if((keys %obj3)>0){
+				push(@{$obj2{$key2}},rmap(\%obj3));
+				%obj3=();
+				$key3="";
+			}
+			push(@{$obj2{$3}}, $5);
 			$key2=$3;
 			$lastobj=$obj2{$3};
-		}elsif($3 =~/^\/([a-z_]+)=(.*)/){
-			push(@{$obj3{$1}},"$2 $value");
+		}elsif($3 =~/^\/([a-z_]+)="{0,1}(.*)$/){
 			$key3=$1;
-			$lastobj=$obj3{$1};
+			$value=$2.$value;
+			$value=~s/"*\s*$//g;
+			push(@{$obj3{$key3}},$value);
+			$lastobj=$obj3{$key3};
 		}else{
-			my $ln=$_;
-			$ln=~s/^\s+|\s+$//g;
-			push(@{$lastobj}, $ln);
+			$ln=~s/^\s+|"*\s+$//g;
+			if(join('',map { ref() eq 'HASH' ? 1 : 0} $lastobj) ==0){
+				${$lastobj}[0].= $ln;
+			}else{
+				push(@{$lastobj}, $ln);
+			}
 		}
 	}
 }
-%obj1=rmap(%obj1);
+%obj1=%{rmap(\%obj1)};
 print to_json(\%obj1,{utf8 => 1, pretty => 1});
 
 sub rmap{
-	my (%obj)=@_;
-	return map { ref() eq 'ARRAY' ? join("\n",@$_) : (ref() eq 'HASH' ? { map {rmap($_)} %$_ } : $_) } %obj;
+	my ($obj)=@_;
+	tie my %res, "Tie::IxHash";
+	%res= map { $_ => (ref($obj->{$_}) eq 'ARRAY' &&  @{$obj->{$_}}==1) ? ${ $obj->{$_} }[0] : $obj->{$_} } keys %{$obj};
+	return \%res;
 }
--- src/updatejson.sh
+++ src/updatejson.sh
@@ -4,5 +4,6 @@ ID=GCF_009858895.2_ASM985889v3
 fn=${ID}_genomic.gbff
 echo ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/009/858/895/$ID/$fn.gz
 wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/009/858/895/$ID/$fn.gz
-gunzip $fn.gz
+gunzip -f $fn.gz
 gbff2json.pl $fn > $fn.json
+mv $fn* ../data
