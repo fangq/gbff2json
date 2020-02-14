1b590ff8960217fa8dce0b8af6a6e48d19595cb3
Qianqian Fang <fangqq@gmail.com>
2020-02-14 14:01:49 -0500

    finally fix hash key orders, add command line options

--- src/gbff2json.pl
+++ src/gbff2json.pl
@@ -5,9 +5,9 @@
 #
 # Author:  Qianqian Fang <q.fang at neu.edu>
 # License: BSD 3-clause
-# Version: 0.4
+# Version: 0.5
 # URL:     http://openjdata.org
-# Github:  https://github.com/fangq/gene2019ncov/
+# Github:  https://github.com/fangq/covid19/
 #
 ###############################################################################
 
@@ -18,16 +18,37 @@ use Tie::IxHash;
 
 if($#ARGV<0){
 	print("gbff2json.pl - converting GenBank database file to a JSON/JData file
-	Format: gbff2json.pl input.gbff > output.json\n");
+	Format: gbff2json.pl <options> input.gbff > output.json
+The supported options include (multiple parameters can be used, separated by spaces)
+	-m	convert key names to MATLAB friendly forms, otherwise, keeps the original key names
+	-C	use all-caps for top-level key names, otherwise, capitalize only the first letter
+	-c	print JSON in compact form, otherwise, print in the indented form\n");
 	exit 0;
 }
 
-my ($key1, $key2, $key3)=("","","");
-my ($lastobj,$value);
+my ($key1, $key2, $key3, $originkey)=("","","","Origin");
+my ($lastobj,$value, $options);
+my %jsonopt=(utf8 => 1, pretty => 1);
 
 tie my %obj1, "Tie::IxHash";
 tie my %obj2, "Tie::IxHash";
 tie my %obj3, "Tie::IxHash";
+
+# parse commandline options, the last input is assumed to be the .gbff file or stdin
+$options='';
+while(my $opt = $ARGV[0]) {
+    if($opt =~ /^-[mcC]$/){
+	$options.='m'         if($opt eq '-m');
+	$jsonopt{'pretty'}=0  if($opt eq '-c');
+	if($opt eq '-C'){
+	    $options.='C';
+	    $originkey="ORIGIN";
+	}
+	shift;
+    }else{
+	last;
+    }
+}
 
 while(<>){ # loop over each line of the input file
 	next if(/^\s*$/);  # skip empty lines
@@ -36,22 +57,22 @@ while(<>){ # loop over each line of the input file
 		$value=$4.$5;
 		if(length($2)==0){ # a first level key start at the begining of the line
 			if((keys %obj3)>0){ # attaching lower-level objects
-				push(@{$obj2{$key2}},rmap(\%obj3));
+				push(@{$obj2{$key2}},rmap(\%obj3,$options));
 				%obj3=();
 				$key3="";
 			}
 			if((keys %obj2)>0){
-				push(@{$obj1{$key1}},rmap(\%obj2));
+				push(@{$obj1{$key1}},rmap(\%obj2,$options));
 				%obj2=();
 				$key2="";
 			}
 			last if(/^\/\/$/);
-			$key1=ucfirst(lc($3));
+			$key1= (!($options=~/C/)) ? ucfirst(lc($3)) : $3;
 			push(@{$obj1{$key1}}, $5) if $5 ne '';
 			$lastobj=$obj1{$3};
 		}elsif(length($2)<12){ # a second level key starts within 12 spaces from the beginning
 			if((keys %obj3)>0){  # attaching lower-level objects
-				push(@{$obj2{$key2}},rmap(\%obj3));
+				push(@{$obj2{$key2}},rmap(\%obj3,$options));
 				%obj3=();
 				$key3="";
 			}
@@ -74,23 +95,47 @@ while(<>){ # loop over each line of the input file
 		}
 	}
 }
-%obj1=%{rmap(\%obj1)};
 
-# concatenate ORIGIN hash values into a single string
-if($obj1{'Origin'}){
-	$obj1{'Origin'}=join('',map { $obj1{'Origin'}{$_}} keys %{$obj1{'Origin'}});
-	$obj1{'Origin'}=~s/\s//g;
+%obj1=%{rmap(\%obj1,$options)};
+
+# concatenate ORIGIN hash values into a single string for easy lookup
+if($obj1{$originkey}){
+	$obj1{$originkey}=join('',map { $obj1{$originkey}{$_}} keys %{$obj1{$originkey}});
+	$obj1{$originkey}=~s/\s//g;
 }
 
-print to_json(\%obj1,{utf8 => 1, pretty => 1});
+# output the final JSON
+
+print to_json(\%obj1,\%jsonopt);
+
+###############################################################################
 
 sub rmap{
-	my ($obj)=@_;
+	my ($obj,$opt)=@_;
 	tie my %res, "Tie::IxHash";
 	%res= map { $_ => (ref($obj->{$_}) eq 'ARRAY' &&  @{$obj->{$_}}>0) 
 	              ? ( @{$obj->{$_}}==1 ? ${ $obj->{$_} }[0] : 
-		           (@{$obj->{$_}} %2 ==0 ? { @{ $obj->{$_} } } : $obj->{$_} ) )
+		           (@{$obj->{$_}} %2 ==0 ? fixkey($obj->{$_},$opt) : $obj->{$_} ) )
 		      : $obj->{$_}
 	         } keys %{$obj};
 	return \%res;
+}
+
+sub fixkey{
+	my ($obj,$opt)=@_;
+	tie my %res, "Tie::IxHash";
+	for (my $i = 0; $i < @{$obj}; $i += 2) {
+	    my($k, $v) = @$obj[$i,$i+1];
+	    $res{validname($k,$opt)}=$v;
+        }
+	return \%res;
+}
+
+# create matlab/octave friendly key names
+sub validname{
+	my ($str,$opt)=@_;
+	return $str if (! ($opt=~ /m/));
+	$str=~s/[\s\/,()]/_/g;  # replace white spaces, commas and "/" by "_" without losing info
+	$str=~s/(\d+)\.\.(\d+)/From_$1_to_$2/g;  # replace start..end pairs by From_start_to_end
+	return $str;
 }
